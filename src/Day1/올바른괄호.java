6c985166a063e27c20e26921765ad8efae900fde
acgrp <acgrp103@naver.com>
2026-05-13 19:59:57 +0900

    no message

--- src/Day1/올바른괄호.java
+++ src/Day1/올바른괄호.java
@@ -3,7 +3,7 @@ package Day1;
 import java.util.Stack;
 
 public class 올바른괄호 {
-    public static void main(String[] args) {
+    public static boolean main(String[] args) {
 
         String s = "()()";
 
